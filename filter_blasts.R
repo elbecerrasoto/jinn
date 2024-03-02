a4f332b4cc8432b68711b87bef2c0581f899c232
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2024-03-02 12:02:26 -0700

    left join of q_pids pid_doms

--- filter_blasts.R
+++ filter_blasts.R
@@ -9,27 +9,53 @@ ISCAN <- "results/iscan.tsv"
 
 # TODO: the filtering data into table
 # Something like
-# q_alias, query, IP_filter, IP_txt 
+# q_alias, query, IP_filter, IP_txt
 QUERIES <- c("WP_003243987.1", "WP_003243213.1")
-QUERIES_ALIAS <- c("YwqJ", "YwqL")
-names(QUERIES_ALIAS) <- QUERIES
-FILTER_DOMAINS <- list(c("IPR027797", "IPR025968"), c("IPR007581"))
-names(FILTER_DOMAINS) <- QUERIES
+QUERIES_ALIAS <- list("YwqJ", "YwqL") |>
+  `names<-`(QUERIES)
+
+FILTER_DOMAINS <- list(c("IPR027797", "IPR025968"), c("IPR007581")) |>
+  `names<-`(QUERIES)
 
 
 blasts <- read_tsv(BLASTS)
 iscan <- read_tsv(ISCAN)
 
+
+
 # Mappings
 # qs -> pids -> doms
+# Use summarise + list to get a different shape
 # Map each query to pid (is a 1-to-many mapping)
-q_pids <- blast |>
+q_pids <- blasts |>
   group_by(qseqid) |>
-  summarise(pids = list(unique(sseqid)))
+  reframe(pid = unique(sseqid)) |>
+  rename(query = qseqid)
+
+
 # Map each pid to domains (is a 1-to-many mapping)
 pid_doms <- iscan |>
   group_by(protein) |>
-  summarise(doms = list(unique(interpro[interpro != "-"])))
+  reframe(domain = unique(interpro[interpro != "-"])) |>
+  rename(pid = protein)
 
 
-pid_doms
+switch_vect <- function(v_chr, switch_list) {
+  f <- function(v_chr) {
+    do.call(
+      switch,
+      c(v_chr, switch_list, NA)
+    )
+  }
+
+  map_chr(v_chr, f)
+}
+
+q_pids <- q_pids |>
+  mutate(q_alias = switch_vect(query, QUERIES_ALIAS)) |>
+  relocate(q_alias)
+
+
+x <- left_join(q_pids, pid_doms)
+
+view(x)
