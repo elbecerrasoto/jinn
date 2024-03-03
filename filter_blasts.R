e63f8949dfe9b4614c27bbecc02c15bc59219777
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2024-03-03 00:58:40 -0700

    pass: filter_blasts
    ---
    Silly assignation timing.
    It should be out of the for loop.

--- filter_blasts.R
+++ filter_blasts.R
@@ -63,57 +63,32 @@ q_pids_domains <- left_join(q_pids, pid_doms) |>
 
 # Filter by domains
 filter_by_domain <- function(pids, qs, doms, filtering_doms) {
+  
   names(doms) <- pids
   names(qs) <- pids
   return_vec <- vector(mode = "logical", length = length(pids))
 
   for (i in seq_along(pids)) {
-    cat("\n")
 
-    print(paste("Starting i is:", i))
     pid <- pids[i]
-    print(paste("pid is", pid))
-    if (pid == "WP_003243987.1") {
-      print(paste("ref id:", pid))
-      print("#################")
-    }
     return_current <- FALSE
 
     for (query in names(filtering_doms)) {
       if (qs[pid] == query) {
-        print(paste("Entre con", query))
-        print(paste("filtering doms query is", filtering_doms[[query]]))
-        print(paste("doms[pid] is", doms[pid]))
-        print(paste("Ending Cycle", i))
+        
         return_current <- all(filtering_doms[[query]] %in% doms[[pid]])
         break()
       }
-
-      return_vec[i] <- return_current
     }
+    return_vec[i] <- return_current
   }
   return_vec
 }
 
+q_pids_domains_filtered <- q_pids_domains |> 
+  filter(filter_by_domain(pid, query, domains, FILTER_DOMAINS))
 
-
-pid <- q_pids_domains$pid
-query <- q_pids_domains$query
-domains <- q_pids_domains$domains
-
-names(domains) <- pid
-
-
-ref <- "GCF_000699465.1"
-
-ref_pids <- blasts |>
-  filter(genome == "GCF_000699465.1") |>
-  pull(sseqid)
-
-all(FILTER_DOMAINS[["WP_003243987.1"]] %in% domains[[ref_pids[1]]])
-
-
-
-
-
-filter_by_domain(pid, query, domains, FILTER_DOMAINS)
+# Filter blasts
+blasts |>
+  filt
+q_pids_domains_filtered$pid
